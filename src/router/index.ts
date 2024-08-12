0a7b89d1fad00f53a3b44610821c9e616783f22c
LinlinWang2王琳琳Softwa <linlin.wang2@tigermedgrp.com>
2024-08-12 16:48:23 +0800

    updated

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/login',
+      redirect: '/'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
+
+export default router
